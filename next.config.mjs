5e044eec63556056aef32091db76de9a273352bb
balajigopalrathnam-max <balajigopalrathnam@gmail.com>
2025-08-31 12:30:55 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   experimental: {
     serverActions: {},
+    // Ensure server bundling of CJS libs like rss-parser
+    serverComponentsExternalPackages: ['rss-parser'],
   },
 };
+
 export default nextConfig;
